9477c27da109960affc4773836f4cac3deb176ec
yehuohan <yehuohan@qq.com>
2020-09-22 12:54:09 +0800

    feat(rust): add info mod

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,8 +4,19 @@
 //! - https://doc.rust-lang.org/book/
 //! - https://github.com/KaiserY/rust-book-chinese
 
-// 声明模块，内容在src/<mod>.rs或src/<mod>/mod.rs文件中
+// 模块声明，内容在src/<mod>.rs或src/<mod>/mod.rs文件中
 // bin(main.rs)使用mod，需要用 use lnrust::<mod>
 // lib(<mod>.rs)相互之间使用mod，可以用 use crate::<mod>
 pub mod basic;
 pub mod ownership;
+pub mod structure;
+
+
+pub mod info {
+    pub fn name() -> &'static str {
+        "lurst"
+    }
+    pub fn ver() -> &'static str {
+        "0.1.0"
+    }
+}
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,7 +1,11 @@
+use lnrust::info;
 use lnrust::basic;
 use lnrust::ownership;
+use lnrust::structure;
 
 pub fn main() {
+    println!("{}, {}", info::name(), info::ver());
     basic::run();
     ownership::run();
+    structure::run();
 }
